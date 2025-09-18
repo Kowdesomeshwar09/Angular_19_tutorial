5afbf427f2bed3de4bd78cc69aeff2c273cf7f46
Kowdesomeshwar09 <kowdesomeshwar@gmail.com>
2025-09-18 12:40:40 +0530

    feat: Update structural directive example to include input handling and conditional rendering

--- src/app/structural-directive-ng-if-vs-if/structural-directive-ng-if-vs-if.ts
+++ src/app/structural-directive-ng-if-vs-if/structural-directive-ng-if-vs-if.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-structural-directive-ng-if-vs-if',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './structural-directive-ng-if-vs-if.html',
   styleUrl: './structural-directive-ng-if-vs-if.scss',
 })
 export class StructuralDirectiveNgIfVsIf {
   isChecked: boolean = false;
   isInputbox: boolean = true;
+  input1: string = '';
+  input2: string = '';
   onClick() {
     this.isChecked = !this.isChecked;
   }
